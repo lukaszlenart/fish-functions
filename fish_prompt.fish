a81fc1bebfd2ea152be2154eda77fd90e602db06
Lukasz Lenart <lukasz.lenart@gmail.com>
2015-06-17 09:36:44 +0200

    Fixes indent

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -16,7 +16,7 @@ function fish_prompt --description 'Write out the prompt'
     echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" ':'
 
     set_color $fish_color_cwd
-        echo -n (prompt_pwd)
+    echo -n (prompt_pwd)
     set_color normal
 
     set_color blue
