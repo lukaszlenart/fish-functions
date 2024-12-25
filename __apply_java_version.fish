4f77df30ee49920582a1b4a63c753c2cfa449721
Lukasz Lenart <lukasz.lenart@gmail.com>
2024-12-25 07:47:57 +0100

    Cleans up old functions

--- __apply_java_version.fish
+++ __apply_java_version.fish
@@ -1,18 +1,18 @@
 function __apply_java_version --description 'Applies Java version based on existing .java-version file'
   if test -e $PWD/.java-version
     set __java_version (cat $PWD/.java-version)
-    if test "$__java_version" = "1.7"
-      jdk7
-    else if test "$__java_version" = "1.8"
+    if test "$__java_version" = "1.8"
       jdk8
     else if test "$__java_version" = "11.0"
       jdk11
     else if test "$__java_version" = "11"
       jdk11
-    else if string match -q '17.*' $__java_version
+    else if string match -q '17*' $__java_version
       jdk17
-    else if string match -q '21.*' $__java_version
+    else if string match -q '21*' $__java_version
       jdk21
+    else if string match -q '22*' $__java_version
+      jdk22
     else
       echo "Uknown version: $__java_version"
     end
--- jdk17.fish
+++ jdk17.fish
@@ -2,7 +2,7 @@ function jdk17 --description "Switches \$JAVA_HOME to JDK17"
   echo "Switching \$JAVA_HOME to JDK17"
 
   set -gx JAVA_HOME (/usr/libexec/java_home -v 17)
-  jenv local 17.0.9
+  jenv local 17
   
   echo "\$JAVA_HOME = $JAVA_HOME"
   java -version
