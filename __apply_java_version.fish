b790b7ca59adda6436b643fb9f869961e350a7d9
Lukasz Lenart <lukasz.lenart@gmail.com>
2025-04-30 15:03:53 +0200

    Adds support for JDK11

--- __apply_java_version.fish
+++ __apply_java_version.fish
@@ -3,6 +3,8 @@ function __apply_java_version --description 'Applies Java version based on exist
     set __java_version (cat $PWD/.java-version)
     if test "$__java_version" = "1.8"
       jdk8
+    else if string match -q '11*' $__java_version
+      jdk11
     else if string match -q '17*' $__java_version
       jdk17
     else if string match -q '21*' $__java_version
